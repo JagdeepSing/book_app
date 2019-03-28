85d763bfec0a4e46ffe5c96b460facbc59161086
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-03-27 21:18:15 -0700

    comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 $(() => {
   'use strict';
 
+  // menu displays on mouseover of menu icon
   $('#menu-icon').on('mouseover', () => {
     $('#menu').show();
     $('header').on('mouseleave', () => {
