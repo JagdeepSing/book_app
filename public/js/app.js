aff69a93f2f30d2429d0dc1d854ceafea3cc8d70
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-03-28 22:38:40 -0700

    details page update button works

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,17 @@ $(() => {
       $('#menu').hide();
     });
   });
+
+  // click out of form to exit form popup
+  $('.form-div').on('click', function(event) {
+    if (event.target === this) {
+      $(this).hide();
+    }
+  });
+
+  // update button on book details page displays popup form
+  $('.update-btn').on('click', function(event) {
+    console.log($(event.target).val());
+    $($(event.target).val()).show();
+  });
 });
